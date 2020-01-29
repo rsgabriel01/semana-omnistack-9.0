2d413ce444a23f32b17be6dd0837d1701a8b9d8a
Gabriel Rodrigues <grsou3@minha.fag.edu.br>
2020-01-29 00:49:50 -0300

    files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -13,7 +15,10 @@ mongoose.connect('mongodb+srv://omnistack9:omnistack9@semanaoministack9-diw8e.mo
 //req.query = Acessar os query params (para filtros)
 //req.params = Acessar os route params (para edição e delete)
 //req.body = Acessa corpo da requisição (para criação, edição)
+
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes)
 
 app.listen(3333);
